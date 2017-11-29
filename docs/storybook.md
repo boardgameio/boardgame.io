4946db914c03a74203254f67144bd42b47fcbe86
Nicolo Davis <nicolodavis@google.com>
2017-11-28 20:39:35 -0800

    add storybook and basic card that does nothing right now

---
+++ docs/storybook.md
@@ -0,0 +1,5 @@
+```react
+<iframe src='storybook/index.html' height='700' scrolling='no' title='example' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'></iframe>
+```
+!> This is a work in progress.
+
---
+++ src/client/lib/card/card.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from 'react';
+import './card.css';
+
+export class Card extends React.Component {
+  render() {
+    return (
+      <div className="bgio-card">
+      Card
+      </div>
+    );
+  }
+}
