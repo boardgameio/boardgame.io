0635854b201b543928740ebcc4847be30a7b2d25
Nicolo Davis <nicolodavis@google.com>
2017-12-16 20:29:35 -0800

    rename TicTacToeBoard to Board and move it to tic-tac-toe.js

--- examples/tic-tac-toe/app.js
+++ examples/tic-tac-toe/app.js
@@ -7,69 +7,14 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 import Client from 'boardgame.io/client';
-import { TicTacToe } from './tic-tac-toe';
+import { TicTacToe, Board } from './tic-tac-toe';
 import './app.css';
-
-class TicTacToeBoard extends React.Component {
-  static propTypes = {
-    G:        PropTypes.any.isRequired,
-    ctx:      PropTypes.any.isRequired,
-    endTurn:  PropTypes.func.isRequired,
-    moves:    PropTypes.any.isRequired,
-  }
-
-  onClick = (id) => {
-    if (this.isActive(id)) {
-      this.props.moves.clickCell(id);
-      this.props.endTurn();
-    }
-  }
-
-  isActive(id) {
-    if (this.props.G.winner != null) return false;
-    if (this.props.G.cells[id] !== null) return false;
-    return true;
-  }
-
-  render() {
-    let tbody = [];
-    for (let i = 0; i < 3; i++) {
-      let cells = [];
-      for (let j = 0; j < 3; j++) {
-        const id = 3 * i + j;
-        cells.push(
-          <td key={id}
-              className={this.isActive(id) ? 'active' : ''}
-              onClick={() => this.onClick(id)}>
-            {this.props.G.cells[id]}
-          </td>
-        );
-      }
-      tbody.push(<tr key={i}>{cells}</tr>);
-    }
-
-    let winner = '';
-    if (this.props.G.winner !== null) {
-      winner = <div id='winner'>Winner: {this.props.G.winner}</div>;
-    }
-
-    return (
-      <div>
-        <table id="board">
-        <tbody>{tbody}</tbody>
-        </table>
-        {winner}
-      </div>
-    );
-  }
-}
 
 export const App = Client({
   game: TicTacToe,
-  board: TicTacToeBoard,
+  board: Board,
   multiplayer: true,
 });
 
