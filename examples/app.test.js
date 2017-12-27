3e436ddf2ab0d091edbe2de1e4c6b87816ebbe20
Nicolo Davis <nicolodavis@google.com>
2017-12-26 18:19:02 -0800

    refactor example directory structure

--- examples/app.test.js
+++ examples/app.test.js
@@ -9,7 +9,7 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { App } from './app';
+import { Singleplayer as App } from './app';
 
 Enzyme.configure({ adapter: new Adapter() });
 
