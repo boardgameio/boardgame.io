e586d0e8d369e0cea28c31b43094b7be764a2e3f
Nicolo Davis <nicolodavis@google.com>
2017-12-26 18:27:21 -0800

    refactor examples

--- examples/tic-tac-toe/index.js
+++ examples/tic-tac-toe/index.js
@@ -8,56 +8,9 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import Game from 'boardgame.io/game';
-
-function IsVictory(cells) {
-  const positions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let pos of positions) {
-    const symbol = cells[pos[0]];
-    let winner = symbol;
-    for (let i of pos) {
-      if (cells[i] != symbol) {
-        winner = null;
-        break;
-      }
-    }
-    if (winner != null) return true;
-  }
-
-  return false;
-}
-
-export const TicTacToe = Game({
-  setup: () => ({
-    cells: Array(9).fill(null)
-  }),
-
-  moves: {
-    clickCell(G, ctx, id) {
-      const cells = [...G.cells];
-
-      if (cells[id] === null) {
-        cells[id] = ctx.currentPlayer;
-      }
-
-      return { ...G, cells };
-    }
-  },
-
-  victory: (G, ctx) => {
-    return IsVictory(G.cells) ? ctx.currentPlayer : null;
-  }
-});
+import { TicTacToe } from './game';
+import Client from 'boardgame.io/client';
+import './index.css';
 
 export class Board extends React.Component {
   static propTypes = {
@@ -123,3 +76,8 @@ export class Board extends React.Component {
     );
   }
 }
+
+export const Singleplayer = Client({
+  game: TicTacToe,
+  board: Board
+});
