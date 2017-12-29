9c01fe13da075b0a3a17af971c427db3ddc9de4f
Nicolo Davis <nicolodavis@google.com>
2017-12-29 13:38:19 -0800

    maintain AUTHORS file and update Copyright to reflect the fact that there are multiple contributors

--- src/core/reducer.js
+++ src/core/reducer.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Google Inc.
+ * Copyright 2017 The boardgame.io Authors
  *
  * Use of this source code is governed by a MIT-style
  * license that can be found in the LICENSE file or at
