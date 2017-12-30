f2f57d8c67eb1385e39a791dbc2e0c682982740d
Nicolo Davis <nicolodavis@google.com>
2017-12-29 22:20:25 -0800

    add test for flow reducer override

--- src/core/flow.test.js
+++ src/core/flow.test.js
@@ -16,9 +16,15 @@ test('basic', () => {
   const reducer = createGameFlow({game});
 
   let state = undefined;
-
   state = reducer(state, {});
   expect(state.turn).toBe(0);
   state = reducer(state, endTurn());
   expect(state.turn).toBe(1);
+});
+
+test('flow reducer override', () => {
+  const flow = () => {};
+  const game = Game({ flow });
+  const reducer = createGameFlow({game});
+  expect(reducer).toBe(flow);
 });
