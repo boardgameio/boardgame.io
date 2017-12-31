3b44641cdec43f0f1793b89b00185f182131d92c
Josh <c13joshua.christman@gmail.com>
2017-12-30 23:36:16 -0500

    - Update test case

--- examples/modules/app.test.js
+++ examples/modules/app.test.js
@@ -9,7 +9,7 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './singleplayer';
+import App from './app';
 
 Enzyme.configure({ adapter: new Adapter() });
 
