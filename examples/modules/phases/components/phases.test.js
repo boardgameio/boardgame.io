52d12fd4c4c69d7fe5129b1ddbb516e4fab98220
Nicolo Davis <nicolodavis@google.com>
2018-01-05 18:42:48 -0800

    phases example

---
+++ examples/modules/phases/components/phases.test.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2018 The boardgame.io Authors
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Phases from './phases';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('basic', () => {
+  const game = Enzyme.mount(<Phases/>);
+  const board = game.find('Board');
+  expect(board.props().G.deck).toBe(5);
+});
---
+++ examples/modules/phases/routes.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2018 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import Phases from './components/phases';
+
+const routes = [
+  {
+    path: '/phases',
+    text: 'Phases',
+    component: Phases
+  }
+];
+
+export default routes;
