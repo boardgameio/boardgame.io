21ef985255d9253359056b6b1438d200641d6064
Nicolo Davis <nicolodavis@google.com>
2018-01-13 23:01:44 -0800

    basic hexgrid

--- storybook/index.js
+++ storybook/index.js
@@ -10,11 +10,19 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Card } from '../src/ui/card';
+import { HexGrid } from '../src/ui/hex';
 
 storiesOf('Card', module)
   .add('basic', () => (
     <div style={{ padding: '50px' }} >
     <Card onHover={action('onHover')}
           onClick={action('onClick')} />
+    </div>
+  ));
+
+storiesOf('HexGrid', module)
+  .add('HexGrid', () => (
+    <div style={{ padding: '50px' }} >
+    <HexGrid radius={5} />
     </div>
   ));
