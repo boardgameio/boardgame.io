145b77464ec87c05e7f89d1311c5f5f1b0e82544
Nicolo Davis <nicolodavis@google.com>
2018-01-13 23:01:44 -0800

    basic hexgrid

---
+++ src/ui/hex.js
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2018 The boardgame.io Authors
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * HexGrid
+ *
+ * Component to display a hex grid.
+ * Inspired by https://www.redblobgames.com/grids/hexagons/.
+ *
+ * Props:
+ *   radius     - The number of levels around the central hex.
+ *   style      - CSS style of the HTML element.
+ *   onClick    - (x, y, z) => {}
+ *                Called when a hex is clicked.
+ *
+ * Usage:
+ *
+ * <HexGrid radius={5}>
+ *   <Token x={0} y={0} z={0}/>
+ * </Grid>
+ */
+export class HexGrid extends React.Component {
+  static propTypes = {
+    radius: PropTypes.number.isRequired,
+    style: PropTypes.object,
+    onClick: PropTypes.func,
+    hexSize: PropTypes.number,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.element),
+        PropTypes.element
+    ]),
+  }
+
+  static defaultProps = {
+    radius: 5,
+    hexSize: 5
+  }
+
+  _getHexes() {
+    let hexes = [];
+    const r = this.props.radius;
+    for (let x = -r; x <= r; x++) {
+      for (let y = -r; y <= r; y++) {
+        const z = - x - y;
+        if (Math.abs(z) > r) continue;
+        hexes.push(
+          <Hex key={`${x}:${y}:${z}`}
+               x={x} y={y} z={z}
+               size={this.props.hexSize} />);
+      }
+    }
+    return hexes;
+  }
+
+  render() {
+    const t = this.props.hexSize * this.props.radius * 2;
+    return (
+      <svg viewBox={-t + ' ' + -t + ' ' + 2 * t + ' ' + 2 * t}
+           style={this.props.style}>
+        <g>{this._getHexes()}</g>
+        {this.props.children}
+      </svg>);
+  }
+}
+
+/**
+ * Hex (flat-topped).
+ *
+ */
+export class Hex extends React.Component {
+  static propTypes = {
+    x: PropTypes.number,
+    y: PropTypes.number,
+    z: PropTypes.number,
+    size: PropTypes.number,
+  }
+
+  static defaultProps = {
+    size: 1,
+    x: 0,
+    y: 0,
+    z: 0,
+  }
+
+  state = {
+    highlight: false
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  get width() {
+    return this.props.size * 2;
+  }
+
+  get height() {
+    return (Math.sqrt(3) / 2 * this.width).toFixed(3);
+  }
+
+  /**
+   * Get the co-ordinates of the hex center.
+   */
+  get center() {
+    const q = this.props.x;
+    const r = this.props.z;
+    const x = this.props.size * 3 * q / 2.0;
+    const y = this.props.size * Math.sqrt(3) * (r + q / 2.0);
+    return { x, y };
+  }
+
+  /**
+   * Get the points of the vertices.
+   */
+  get points() {
+    //   b____c
+    //   /    \
+    // a/      \d
+    //  \      /
+    //   \____/
+    //   f    e
+
+    const s = this.props.size;
+    const h = this.height;
+
+    const xa = -s;
+    const xb = -s / 2.0;
+    const xc = +s / 2.0;
+    const xd = +s;
+    const xe = xc;
+    const xf = xb;
+
+    const ya = 0.0;
+    const yb = h / 2.0;
+    const yc = yb;
+    const yd = ya;
+    const ye = -h / 2.0;
+    const yf = ye;
+
+    const flatTop = [
+      `${xa},${ya}`,
+      `${xb},${yb}`,
+      `${xc},${yc}`,
+      `${xd},${yd}`,
+      `${xe},${ye}`,
+      `${xf},${yf}`,
+    ];
+
+    return flatTop.join(' ');
+  }
+
+  onClick = () => {
+    this.setState({ highlight: true });
+  }
+
+  render() {
+    const sx = this.center.x;
+    const sy = this.center.y;
+
+    const fill = this.state.highlight ? '#aaa' : '#fff';
+
+    return (
+      <g className="hex"
+         transform={`translate(${sx}, ${sy})`}>
+        <polygon points={this.points}
+                 onClick={this.onClick}
+                 fill={fill}
+                 stroke="#aaa"
+                 strokeWidth={.1}/>
+      </g>
+    );
+  }
+}
