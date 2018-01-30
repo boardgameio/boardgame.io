3770436bc8c30843eedeba980c3094018cab7495
Chris Heninger <chris@igravitystudios.com>
2018-01-29 19:14:43 -0700

    Add a couple more tests for deck

--- src/ui/deck.test.js
+++ src/ui/deck.test.js
@@ -15,10 +15,31 @@ import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
 
 test("basic", () => {
+  const cards = [<Card key={0} />, <Card key={1} />];
+  const deck = Enzyme.mount(<Deck cards={cards} />);
+  expect(deck.html()).toContain("svg");
+});
+
+test("handlers", () => {
+  const cards = [<Card key={0} />, <Card key={1} />];
+  const onClick = jest.fn();
+  const deck = Enzyme.mount(<Deck onClick={onClick} cards={cards} />);
+
+  deck.simulate("click");
+  expect(onClick).toHaveBeenCalled();
+});
+
+test("splayWidth", () => {
   const cards = [
     <Card key={0} />,
     <Card key={1} />,
+    <Card key={2} />,
+    <Card key={3} />
   ];
-  const deck = Enzyme.mount(<Deck cards={cards}/>);
-  expect(deck.html()).toContain('svg');
+  const splayWidth = 10;
+  const deck = Enzyme.mount(<Deck cards={cards} splayWidth={splayWidth} />);
+
+  deck.find(".bgio-card").forEach((node, index) => {
+    expect(node.props().style.left).toEqual(splayWidth * index);
+  });
 });
