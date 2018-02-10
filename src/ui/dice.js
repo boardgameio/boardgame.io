b61d7aab1a8d2e0bc26afc93dffe73fbc3ec84b7
Matthew Weatherford <weatherfordmat@gmail.com>
2018-02-09 18:27:54 -0600

    build dice 'roll method'

--- src/ui/dice.js
+++ src/ui/dice.js
@@ -10,22 +10,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './dice.css';
 
-const Dice = ({ faceCount, faceValue }) => {
-    const classNames = ['bgio-dice'];
+class Dice extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      faceValue: props.faceValue,
+    };
+  }
+
+  onClick = () => {
+    let { faceCount } = this.props;
+    let faceValue = Math.floor(Math.random() * faceCount) + 1; // can't have a zero face;
+
+    this.setState({
+      faceValue,
+    });
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.faceValue !== nextProps.faceValue) {
+      this.setState({ faceValue: nextProps.faceValue });
+    }
+  }
+
+  render() {
+    let { className, dotStyle, faceCount, ...rest } = this.props;
+    let { faceValue } = this.state;
+    const classNames = ['bgio-dice'];
+    if (className) classNames.push(className);
     return (
-        <div className={classNames.join(' ')} {...rest}>
-            {faceValue}
-        </div>
+      <div className={classNames.join(' ')} {...rest} onClick={this.onClick}>
+        {dotStyle && faceCount < 9 // hard to fit mor ethan 9 "dots" on the dice;
+          ? [...Array(faceValue)].map((face, i) => {
+              return <div key={i} className="bgio-dot" />;
+            })
+          : { faceValue }}
+      </div>
     );
-};
+  }
+}
 
 Dice.propTypes = {
-    faceCount: PropTypes.number,
-    faceValue: PropTypes.number
+  faceCount: PropTypes.number,
+  faceValue: PropTypes.number,
+  dotStyle: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 Dice.defaultProps = {
-    faceCount: 6,
-    faceValue: 1
+  faceCount: 6,
+  faceValue: 6,
+  dotStyle: true,
 };
+
+export default Dice;
