2721fb6f9fb378ab4ee852089a71d18e74ae5c0e
Nicolo Davis <nicolodavis@google.com>
2018-02-17 00:31:05 -0800

    merge randomeval.js into random.js

--- src/core/random.js
+++ src/core/random.js
@@ -6,7 +6,73 @@
  * https://opensource.org/licenses/MIT.
  */
 
-import { addrandomop, DICE, NUMBER } from './randomeval';
+import seedrandom from 'seedrandom';
+
+export const DICE = 'DICE';
+export const NUMBER = 'NUMBER';
+
+function getrandomfn(ctx) {
+  let randomfn;
+  if (ctx.prngstate === undefined) {
+    // no call to a random function has been made.
+    // pre-populate the state info
+    randomfn = new seedrandom.alea(ctx.seed, { state: true });
+  } else {
+    randomfn = new seedrandom.alea('', { state: ctx.prngstate });
+  }
+  return randomfn;
+}
+
+export function randomctx(ctx) {
+  const r = getrandomfn(ctx);
+  const randomnumber = r();
+  const ctx2 = { ...ctx, prngstate: r.state() };
+  return { randomnumber, ctx: ctx2 };
+}
+
+export function addrandomop(G, fieldname, op, ...args) {
+  let rop = [{ op, fieldname, args }];
+  let _randomOps = [...(G._randomOps || []), ...rop];
+  return { ...G, _randomOps };
+}
+
+export function evaluaterandomops(G, ctx) {
+  let randomresults = {};
+  let ctx2 = ctx;
+
+  // some flow tests run without a defined G
+  if (G && G._randomOps !== undefined) {
+    G._randomOps.forEach(r => {
+      const { ctx: ctx3, randomnumber } = randomctx(ctx2);
+      ctx2 = ctx3;
+
+      switch (r.op) {
+        case DICE: {
+          const spotvalue = r.args[0];
+          const dievalue = Math.floor(randomnumber * spotvalue) + 1;
+          randomresults[r.fieldname] = dievalue;
+          break;
+        }
+
+        case NUMBER: {
+          randomresults[r.fieldname] = randomnumber;
+          break;
+        }
+
+        default:
+          break;
+      }
+    });
+  }
+
+  return { randomresults, ctx: ctx2 };
+}
+
+export function RunRandom(G, ctx) {
+  let { randomresults, ctx: ctx2 } = evaluaterandomops(G, ctx);
+  const G2 = { ...G, ...randomresults, _randomOps: undefined };
+  return { G: G2, ctx: ctx2 };
+}
 
 const SpotValue = {
   D4: 4,
@@ -26,11 +92,19 @@ for (const key in SpotValue) {
   };
 }
 
+export function GenSeed() {
+  return Math.seedrandom();
+}
+
+// Public API that's imported like:
+// import { Random } from 'boardgame.io/core';
 export const Random = {
   ...predefined,
+
   Die: (G, fieldname, spotvalue) => {
     return addrandomop(G, fieldname, DICE, spotvalue);
   },
+
   Number: (G, fieldname) => {
     return addrandomop(G, fieldname, NUMBER);
   },
