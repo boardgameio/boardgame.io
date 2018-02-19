6cd25fec8b5b753501c7fea397f67d2afedc840d
Stefan Hanke <stefan.hanke@gmx.org>
2018-02-19 17:18:06 +0100

    allow to specify meeple color, add second HexGrid storyboard

--- storybook/hex.js
+++ storybook/hex.js
@@ -24,51 +24,87 @@ function Basic() {
       this.setState(args);
       action('onClick')(args);
     };
-    render = () => (
-      <HexGrid levels={levels} outline={outline} onClick={this.onClick}>
-        <Token
-          x={this.state.x}
-          y={this.state.y}
-          z={this.state.z}
-          animate={animate}
-          // no template provided => HexGrid will inject Hex by default.
-          style={{ fill: '#555' }}
-        />
+    render = () => {
+      return (
+        <HexGrid levels={levels} outline={outline} onClick={this.onClick}>
+          <Token
+            x={this.state.x}
+            y={this.state.y}
+            z={this.state.z}
+            animate={animate}
+            // no template provided => HexGrid will inject Hex by default.
+            style={{ fill: '#555' }}
+          />
+        </HexGrid>
+      );
+    };
+  }
 
+  return (
+    <div style={{ padding: '50px' }}>
+      <Runner />
+    </div>
+  );
+}
+
+function BasicTokens() {
+  const levels = number('levels', 5);
+  const outline = boolean('outline', true);
+  const animate = boolean('animate', true);
+
+  class Runner extends React.Component {
+    state = { x: 0, y: 0, z: 0 };
+    onClick = args => {
+      this.setState(args);
+      action('onClick')(args);
+    };
+    render = () => {
+      const meepleParade = ['blue', 'green', 'yellow', 'red'].map(
+        (color, i) => (
+          <Token
+            x={i}
+            y={0}
+            z={-1}
+            key={i}
+            template={props => (
+              <SvgComponent
+                {...props}
+                component={Models.Meeple({ color: color })}
+              />
+            )}
+          />
+        )
+      );
+
+      const otherTokens = ['Wood', 'Wheat', 'Disc3D'].map((modelname, i) => (
         <Token
-          x={0}
-          y={0}
-          z={0}
-          template={props => (
-            <SvgComponent {...props} component={Models.Meeple} />
-          )}
-        />
-        <Token
-          x={0}
+          x={i}
           y={0}
           z={1}
+          key={i + 1000}
           template={props => (
-            <SvgComponent {...props} component={Models.Wood} />
+            <SvgComponent {...props} component={Models[modelname]} />
           )}
         />
-        <Token
-          x={0}
-          y={0}
-          z={2}
-          template={props => (
-            <SvgComponent {...props} component={Models.Wheat} />
-          )}
-        />
-        <Token
-          x={0}
-          y={0}
-          z={3}
-          template={props => (
-            <SvgComponent {...props} component={Models.Disc3D} />
-          )}
-        />
-      </HexGrid>
-    );
+      ));
+
+      return (
+        <HexGrid levels={levels} outline={outline} onClick={this.onClick}>
+          <Token
+            key="-1"
+            x={this.state.x}
+            y={this.state.y}
+            z={this.state.z}
+            animate={animate}
+            // no template provided => HexGrid will inject Hex by default.
+            style={{ fill: '#555' }}
+          />
+
+          {meepleParade}
+          {otherTokens}
+        </HexGrid>
+      );
+    };
   }
 
   return (
@@ -80,4 +116,5 @@ function Basic() {
 
 storiesOf('HexGrid', module)
   .addDecorator(withKnobs)
-  .add('basic', Basic);
+  .add('basic', Basic)
+  .add('tokens', BasicTokens);
