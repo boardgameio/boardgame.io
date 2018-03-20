39b1229b17036ca2da52fbd263ceb70b6914fc03
Stefan Hanke <stefan.hanke@gmx.org>
2018-03-20 19:34:31 +0100

    add start of visualization

--- examples/modules/turnorder/components/board.js
+++ examples/modules/turnorder/components/board.js
@@ -9,10 +9,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Board = ({ G }) => <pre>{JSON.stringify(G, null, 2)}</pre>;
+const Board = ({ ctx, events, playerID }) => (
+  <div>
+    <table>
+      <tbody>
+        <tr>
+          <td>playerID</td>
+          <td>
+            <pre>{JSON.stringify(playerID)}</pre>
+          </td>
+        </tr>
+        <tr>
+          <td>currentPlayer</td>
+          <td>
+            <pre>{JSON.stringify(ctx.currentPlayer)}</pre>
+          </td>
+        </tr>
+        <tr>
+          <td>PlayOrder</td>
+          <td>
+            <pre>{JSON.stringify(ctx.playOrder)}</pre>
+          </td>
+        </tr>
+        <tr>
+          <td>actionPlayers</td>
+          <td>
+            <pre>{JSON.stringify(ctx.actionPlayers)}</pre>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div className="buttons">
+      <button
+        {...{ disabled: playerID !== ctx.currentPlayer }}
+        onClick={() => events.endTurn()}
+      >
+        endTurn
+      </button>
+    </div>
+  </div>
+);
 
 Board.propTypes = {
-  G: PropTypes.any.isRequired,
+  ctx: PropTypes.any,
+  events: PropTypes.any,
+  playerID: PropTypes.any,
 };
 
 export default Board;
