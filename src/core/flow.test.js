397aa6a349fdac8dedb040f84ef79c1a03ea51d1
kyle roux <jstacoder@gmail.com>
2018-03-20 22:12:43 -0700

    upated an allowed moves test to use a function

--- src/core/flow.test.js
+++ src/core/flow.test.js
@@ -419,7 +419,7 @@ test('canMakeMove', () => {
 
     flow: {
       phases: [
-        { name: 'A', allowedMoves: 'A' },
+        { name: 'A', allowedMoves: () => ['A'] },
         { name: 'B', allowedMoves: 'B' },
         { name: 'C' },
       ],
