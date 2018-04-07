1bd96e741db77fbcd1b503dea0971f7881db306f
Nicolo Davis <nicolodavis@google.com>
2018-04-07 22:02:03 +0800

    fix rn-cli.config.js

--- examples-react-native/rn-cli.config.js
+++ examples-react-native/rn-cli.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 module.exports = {
-  getProjectRoots: () => [__dirname, path.join(__dirname, '../..')],
+  getProjectRoots: () => [__dirname, path.join(__dirname, '..')],
 };
