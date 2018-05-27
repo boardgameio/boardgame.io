b0b40895067fcedbdab574981335057635058a6d
Nicolo Davis <nicolodavis@google.com>
2018-05-27 20:52:07 +0800

    add missing headers

--- src/server/api-server.js
+++ src/server/api-server.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2018 The boardgame.io Authors
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const koaBody = require('koa-body');
