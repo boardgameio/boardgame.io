caf2a56bd17e60d3face8b77142a52d42abc87b6
korla <bengtsson.johannes@gmail.com>
2018-08-27 23:21:50 +0200

    getRange implemented and tested

--- src/ui/hexUtils.js
+++ src/ui/hexUtils.js
@@ -6,6 +6,8 @@
  * https://opensource.org/licenses/MIT.
  */
 
+const addPoint = (a, b) => ({ x: a.x + b.x, y: a.y + b.y, z: a.z + b.z });
+
 /**
  * Get neighbors
  *
@@ -13,16 +15,13 @@
  * expressed in cube coordinates
  *
  * Arguments:
- *   An object with:
- *   x       - X coordinate (cube coordinates)
- *   y       - Y coordinate (cube coordinates)
- *   z       - Z coordinate (cube coordinates)
+ *   point      (Cube coorinates)
  *
  */
-export const getAllNeighbors = ({ x, y, z }) =>
-  [[1, -1, 0], [1, 0, -1], [0, 1, -1], [0, -1, 1], [-1, 1, 0], [-1, 0, 1]]
-    .map(([dx, dy, dz]) => [x + dx, y + dy, z + dz])
-    .map(([x, y, z]) => ({ x, y, z }));
+export const getAllNeighbors = point =>
+  [[1, -1, 0], [1, 0, -1], [0, 1, -1], [0, -1, 1], [-1, 1, 0], [-1, 0, 1]].map(
+    ([dx, dy, dz]) => addPoint(point, { x: dx, y: dy, z: dz })
+  );
 
 /**
  * Get distance
@@ -39,5 +38,29 @@ export const getAllNeighbors = ({ x, y, z }) =>
  */
 export const getDistance = (a, b) =>
   (Math.abs(a.x - b.x) + Math.abs(a.y - b.y) + Math.abs(a.z - b.z)) / 2;
+
+/**
+ * Get range
+ *
+ * A utility function which returns all points within a range
+ * from the center
+ *
+ * Arguments:
+ *   center    (Cube coordinates)
+ *   distance  number
+ *
+ */
+export const getRange = (center, distance) => {
+  const results = [];
+  for (let x = -distance; x <= distance; x++) {
+    const startY = Math.max(-distance, -x - distance);
+    const stopY = Math.min(distance, -x + distance);
+    for (let y = startY; y <= stopY; y++) {
+      const z = -x - y;
+      results.push(addPoint(center, { x, y, z }));
+    }
+  }
+  return results;
+};
 
 export const hexUtils = { getAllNeighbors, getDistance };
