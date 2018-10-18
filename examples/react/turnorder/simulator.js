b604b953b5ce749b81c8a952d44584492f9bd61e
Nicolo Davis <nicolodavis@gmail.com>
2018-10-19 00:09:53 +0800

    merge militia example into turn order simulator

--- examples/react/turnorder/simulator.js
+++ examples/react/turnorder/simulator.js
@@ -18,66 +18,125 @@ class Board extends React.Component {
     ctx: PropTypes.any.isRequired,
     moves: PropTypes.any,
     events: PropTypes.any,
+    playerID: PropTypes.any,
   };
 
   render() {
-    let players = [];
-    for (let i = 0; i < this.props.ctx.numPlayers; i++) {
-      let className = 'player';
+    let className = 'player';
+    let active = false;
+    let current = false;
+    let onClick = () => {};
 
-      if (i + '' == this.props.ctx.currentPlayer) {
-        className += ' current';
-      }
-
-      if (this.props.ctx.actionPlayers.includes(i + '')) {
-        className += ' active';
-      }
-
-      players.push(
-        <div key={i} className={className}>
-          {i}
-        </div>
-      );
+    if (this.props.ctx.actionPlayers.includes(this.props.playerID)) {
+      className += ' active';
+      active = true;
     }
 
-    return (
-      <div className="turnorder-simulator">
-        <div className="player-container">
-          {players}
+    if (this.props.playerID == this.props.ctx.currentPlayer) {
+      className += ' current';
+      current = true;
+    }
 
-          <div className="end-turn" onClick={this.props.events.endTurn}>
-            End Turn
-          </div>
+    const moves = Object.entries(this.props.moves).map(e => (
+      <button key={e[0]} onClick={e[1]}>
+        {e[0]}
+      </button>
+    ));
+
+    const events = Object.entries(this.props.events)
+      .filter(e => e[0] != 'setActionPlayers')
+      .filter(e => current || e[0] != 'endTurn')
+      .map(e => (
+        <button key={e[0]} onClick={e[1]}>
+          {e[0]}
+        </button>
+      ));
+
+    return (
+      <div>
+        <span className={className} onClick={onClick}>
+          {this.props.playerID}
+        </span>
+
+        <div className="controls">
+          {active && moves}
+          {events}
         </div>
       </div>
     );
   }
 }
 
+const Default = () => <div />;
+
+const Militia = () => <div />;
+
+const examples = {
+  default: {
+    desc: Default,
+    game: Game({}),
+  },
+
+  militia: {
+    desc: Militia,
+    game: Game({
+      flow: { setActionPlayers: true },
+
+      moves: {
+        play(G, ctx) {
+          ctx.events.setActionPlayers({ allOthers: true, once: true });
+          return G;
+        },
+
+        discard(G) {
+          return G;
+        },
+      },
+    }),
+  },
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.init();
+    this.init('militia');
   }
 
-  init() {
-    const game = Game({
-      moves: {},
-    });
+  init(type) {
+    this.desc = examples[type].desc;
 
     this.client = Client({
-      game,
+      game: examples[type].game,
       numPlayers: 6,
       debug: false,
       board: Board,
+      multiplayer: { local: true },
     });
 
     this.forceUpdate();
   }
 
   render() {
-    const C = this.client;
-    return <C />;
+    const Description = this.desc;
+    const App = this.client;
+
+    let players = [];
+    for (let i = 0; i < 6; i++) {
+      players.push(<App key={i} playerID={i + ''} />);
+    }
+
+    return (
+      <div className="turnorder-simulator">
+        <div className="options">
+          <button onClick={() => this.init('default')}>default</button>
+          <button onClick={() => this.init('militia')}>militia</button>
+        </div>
+
+        <Description />
+
+        <div className="player-container">{players}</div>
+      </div>
+    );
   }
 }
 
