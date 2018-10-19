3d9be64675530a2435c9ad0b6c2148b95ec409f9
Nicolo Davis <nicolodavis@gmail.com>
2018-10-19 20:50:49 +0800

    fix typo

--- examples/react/turnorder/example-militia.js
+++ examples/react/turnorder/example-militia.js
@@ -42,7 +42,7 @@ const Description = () => (
   <div>
     <p>
       This is an example from the card game Dominion. The Militia card forces
-      every other play to discard a card.
+      every other player to discard a card.
     </p>
     <pre>{code}</pre>
   </div>
