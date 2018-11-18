2d0255802106823e960a3cdfa130e5c97623c784
Nicolo Davis <nicolodavis@gmail.com>
2018-11-18 22:20:30 +0800

    add TurnOrder.CUSTOM and TurnOrder.CUSTOM_FROM

---
+++ examples/react/turnorder/example-custom-from.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from 'react';
+import { Game, TurnOrder } from 'boardgame.io/core';
+
+const code = `{
+  setup: () => ({
+    order: ['1', '0', '2', '3', '5', '4'],
+  }),
+
+  flow: {
+    turnOrder: TurnOrder.CUSTOM_FROM('order'),
+  },
+}
+`;
+
+const Description = () => (
+  <div>
+    <pre>{code}</pre>
+  </div>
+);
+
+export default {
+  description: Description,
+  game: Game({
+    setup: () => ({
+      order: ['1', '0', '2', '3', '5', '4'],
+    }),
+
+    flow: {
+      endPhase: false,
+      turnOrder: TurnOrder.CUSTOM_FROM('order'),
+    },
+  }),
+};
