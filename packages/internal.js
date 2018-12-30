b8ec84560f3f7d951e0c82e8d489c7ecaeb9a1cc
Nicolo Davis <nicolodavis@gmail.com>
2018-12-30 10:35:01 +0800

    package refactor
    - move Debug from boardgame.io/react to boardgame.io/dist/internal
    - properly export boardgame.io/ai-visualize and boardgame.io/master

--- packages/internal.js
+++ packages/internal.js
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 The boardgame.io Authors
+ * Copyright 2018 The boardgame.io Authors
  *
  * Use of this source code is governed by a MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
 
-import { Client } from '../src/client/react.js';
+import { Debug } from '../src/client/debug/debug.js';
 
-export { Client };
+export { Debug };
