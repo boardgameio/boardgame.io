01a7e79f6bbd218879c6da665c50f27aec9f2f49
Harvey Li <aide0740@gmail.com>
2019-02-15 09:25:26 -0500

    3D Grid and Token (#352)

--- examples/react-web/config/webpack.config.js
+++ examples/react-web/config/webpack.config.js
@@ -322,6 +322,15 @@ module.exports = function(webpackEnv) {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
             },
+            {
+              test: [/\.bin$/],
+              // exclude hash in bin to enable gltf loading
+              loader: require.resolve('url-loader'),
+              options: {
+                limit: 10000,
+                name: 'static/media/[name].[ext]',
+              },
+            },
             // Process application JS with Babel.
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
