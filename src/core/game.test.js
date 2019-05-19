65472e155f440339ee54976a38821c82784b3750
Nicolo Davis <nicolodavis@gmail.com>
2019-05-19 15:56:15 +0800

    refactor some tests

--- src/core/game.test.js
+++ src/core/game.test.js
@@ -7,51 +7,62 @@
  */
 
 import Game from './game';
-import { InitializeGame, CreateGameReducer } from './reducer';
-import { gameEvent } from './action-creators';
+import { Client } from '../client/client';
 
-const game = Game({
-  moves: {
-    A: G => G,
-    B: () => null,
-  },
+describe('basic', () => {
+  let game;
+  beforeAll(() => {
+    game = Game({
+      moves: {
+        A: G => G,
+        B: () => null,
+      },
 
-  flow: {
-    phases: {
-      PA: {
-        moves: {
-          A: () => 'PA.A',
+      flow: {
+        phases: {
+          PA: {
+            moves: {
+              A: () => 'PA.A',
+            },
+          },
         },
       },
-    },
-  },
-});
-
-test('basic', () => {
-  expect(game.moveNames).toEqual(['A', 'B', 'PA.A']);
-  expect(typeof game.processMove).toEqual('function');
-});
-
-test('processMove', () => {
-  const testObj = { test: true };
-  expect(game.processMove(testObj, { type: 'A' })).toEqual(testObj);
-  expect(game.processMove(testObj, { type: 'C' })).toEqual(testObj);
-  expect(game.processMove(testObj, { type: 'B' })).toEqual(null);
-  expect(game.processMove(testObj, { type: 'PA.A' })).toEqual('PA.A');
-});
-
-test('flow override', () => {
-  const f = { processGameEvent: () => {} };
-  const game = Game({
-    flow: f,
+    });
   });
-  expect(game.flow).toBe(f);
+
+  test('sanity', () => {
+    expect(game.moveNames).toEqual(['A', 'B', 'PA.A']);
+    expect(typeof game.processMove).toEqual('function');
+  });
+
+  test('processMove', () => {
+    const testObj = { test: true };
+    expect(game.processMove(testObj, { type: 'A' })).toEqual(testObj);
+    expect(game.processMove(testObj, { type: 'C' })).toEqual(testObj);
+    expect(game.processMove(testObj, { type: 'B' })).toEqual(null);
+    expect(game.processMove(testObj, { type: 'PA.A' })).toEqual('PA.A');
+  });
+
+  test('flow override', () => {
+    const f = { processGameEvent: () => {} };
+    const game = Game({
+      flow: f,
+    });
+    expect(game.flow).toBe(f);
+  });
 });
 
 // Following turn order is often used in worker placement games like Agricola and Viticulture.
 test('rounds with starting player token', () => {
-  let game = Game({
+  const game = Game({
     setup: () => ({ startingPlayerToken: 0 }),
+
+    moves: {
+      takeStartingPlayerToken: (G, ctx) => {
+        G.startingPlayerToken = ctx.currentPlayer;
+      },
+    },
+
     flow: {
       startingPhase: 'main',
       phases: {
@@ -68,35 +79,34 @@ test('rounds with starting player token', () => {
   });
 
   const numPlayers = 4;
-  const reducer = CreateGameReducer({ game });
-  let state = InitializeGame({ game, numPlayers });
+  const client = Client({ game, numPlayers });
 
-  expect(state.ctx.currentPlayer).toBe('0');
+  expect(client.getState().ctx.currentPlayer).toBe('0');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('1');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('1');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('2');
-  // a player took starting player token
-  state = { ...state, G: { startingPlayerToken: 2 } };
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('3');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('2');
 
-  state = reducer(state, gameEvent('endTurn'));
-  state = reducer(state, gameEvent('endPhase', { next: 'main' }));
-  expect(state.ctx.currentPlayer).toBe('2');
+  client.moves.takeStartingPlayerToken();
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('3');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('3');
+  client.events.endTurn();
+  client.events.endPhase({ next: 'main' });
+  expect(client.getState().ctx.currentPlayer).toBe('2');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('0');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('3');
+
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('0');
 });
 
 // The following pattern is used in Catan, Twilight Imperium, and (sort of) Powergrid.
 test('serpentine setup phases', () => {
-  let game = Game({
+  const game = Game({
     flow: {
       startingPhase: 'first setup round',
       phases: {
@@ -131,79 +141,78 @@ test('serpentine setup phases', () => {
   });
 
   const numPlayers = 4;
-  const reducer = CreateGameReducer({ game });
-  let state = InitializeGame({ game, numPlayers });
+  const client = Client({ game, numPlayers });
 
-  expect(state.ctx.currentPlayer).toBe('0');
-  expect(state.ctx.phase).toBe('first setup round');
+  expect(client.getState().ctx.currentPlayer).toBe('0');
+  expect(client.getState().ctx.phase).toBe('first setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('1');
-  expect(state.ctx.phase).toBe('first setup round');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('1');
+  expect(client.getState().ctx.phase).toBe('first setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('2');
-  expect(state.ctx.phase).toBe('first setup round');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('2');
+  expect(client.getState().ctx.phase).toBe('first setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('3');
-  expect(state.ctx.phase).toBe('first setup round');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('3');
+  expect(client.getState().ctx.phase).toBe('first setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  state = reducer(state, gameEvent('endPhase'));
-  expect(state.ctx.currentPlayer).toBe('3');
-  expect(state.ctx.phase).toBe('second setup round');
+  client.events.endTurn();
+  client.events.endPhase();
+  expect(client.getState().ctx.currentPlayer).toBe('3');
+  expect(client.getState().ctx.phase).toBe('second setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('2');
-  expect(state.ctx.phase).toBe('second setup round');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('2');
+  expect(client.getState().ctx.phase).toBe('second setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('1');
-  expect(state.ctx.phase).toBe('second setup round');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('1');
+  expect(client.getState().ctx.phase).toBe('second setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('0');
-  expect(state.ctx.phase).toBe('second setup round');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('0');
+  expect(client.getState().ctx.phase).toBe('second setup round');
 
-  state = reducer(state, gameEvent('endTurn'));
-  state = reducer(state, gameEvent('endPhase'));
-  expect(state.ctx.currentPlayer).toBe('0');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  client.events.endPhase();
+  expect(client.getState().ctx.currentPlayer).toBe('0');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('1');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('1');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('2');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('2');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('3');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('3');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('0');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('0');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('1');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('1');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('2');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('2');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('3');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('3');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('0');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('0');
+  expect(client.getState().ctx.phase).toBe('main phase');
 
-  state = reducer(state, gameEvent('endTurn'));
-  expect(state.ctx.currentPlayer).toBe('1');
-  expect(state.ctx.phase).toBe('main phase');
+  client.events.endTurn();
+  expect(client.getState().ctx.currentPlayer).toBe('1');
+  expect(client.getState().ctx.phase).toBe('main phase');
 });
