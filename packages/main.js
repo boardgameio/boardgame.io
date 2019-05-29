eb8a68e6d91e6fde05921cfb85418c535a9e9130
Nicolo Davis <nicolodavis@gmail.com>
2019-05-29 22:47:49 +0800

    fix import

--- packages/main.js
+++ packages/main.js
@@ -9,7 +9,7 @@
 import { Client } from '../src/client/client.js';
 import { Client as ReactClient } from '../src/client/react.js';
 import { Client as ReactNativeClient } from '../src/client/react-native.js';
-import Game from '../src/core/game.js';
+import { Game } from '../src/core/game.js';
 import { Flow, FlowWithPhases } from '../src/core/flow.js';
 import { TurnOrder, Pass } from '../src/core/turn-order.js';
 import { Token } from '../src/ui/token.js';
