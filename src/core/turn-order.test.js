194c2ba9a67f84b537ad51bce7fdc82cc32ba60e
delucis <swithinbank@gmail.com>
2019-08-21 14:14:20 +0200

    test: Add failing test for OTHERS_ONCE stage default

--- src/core/turn-order.test.js
+++ src/core/turn-order.test.js
@@ -172,6 +172,8 @@ describe('turn orders', () => {
 
     expect(state.ctx.currentPlayer).toBe('1');
     expect(state.ctx.stage).toBeNull();
+
+    state = flow.processMove(state, makeMove('', null, '1').payload);
   });
 
   test('CUSTOM', () => {
