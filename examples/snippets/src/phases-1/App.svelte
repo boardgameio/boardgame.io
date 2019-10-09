20dd281e05b5469798b4792861d573718ecf57cf
Nicolo Davis <nicolodavis@gmail.com>
2019-10-09 20:17:01 +0800

    update phases-1 example to Svelte

---
+++ examples/snippets/src/phases-1/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Player from './Player.svelte';
+</script>
+
+<style>
+  #board {
+    width: 100%;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+</style>
+
+<div>
+  <div id="board">
+    <Player />
+  </div>
+  <div class="container">
+    <Player playerID="0" />
+    <Player playerID="1" />
+    <Player playerID="2" />
+  </div>
+</div>
