3d1c07c55eb74f5c685c7c7e2f9c918d43de117f
Chris Swithinbank <swithinbank@gmail.com>
2020-04-15 03:36:33 +0200

    build(server): Proxy server module with package.json (#622)
    * build(server): Proxy server module with package.json
    
    Mirrors how the other subpackages are bundled by exposing 
    `boardgame.io/server` via a directory with a package.json that includes 
    a pointer to the main entrypoint and server type definitions.
    
    * style(scripts): Fix lint error
    
    * refactor(scripts): Improve implementation of PackgeJson function

--- scripts/proxy-dirs.js
+++ scripts/proxy-dirs.js
@@ -10,24 +10,27 @@ const subpackages = require('../subpackages');
 const path = require('path');
 const { mkdirSync, writeFileSync } = require('fs');
 
-function PackageJson(name) {
-  return `{
-  "name": "boardgame.io/${name}",
-  "private": true,
-  "types": "../dist/types/packages/${name}.d.ts",
-  "main": "../dist/cjs/${name}.js",
-  "module": "../dist/esm/${name}.js"
+function PackageJson(name, { mainDir, esmDir } = {}) {
+  const pkg = {
+    name: `boardgame.io/${name}`,
+    private: true,
+    types: `../dist/types/packages/${name}.d.ts`,
+    main: path.join(mainDir, `${name}.js`),
+  };
+  if (esmDir) {
+    pkg.module = path.join(esmDir, `${name}.js`);
+  }
+  return JSON.stringify(pkg, null, 2) + '\n';
 }
-`;
+
+function makeSubpackage(name, opts) {
+  const dir = path.resolve(__dirname, `../${name}`);
+  mkdirSync(dir);
+  writeFileSync(`${dir}/package.json`, PackageJson(name, opts));
 }
 
 subpackages.forEach(name => {
-  const dir = path.resolve(__dirname, `../${name}`);
-  mkdirSync(dir);
-  writeFileSync(`${dir}/package.json`, PackageJson(name));
+  makeSubpackage(name, { mainDir: '../dist/cjs', esmDir: '../dist/esm' });
 });
 
-writeFileSync(
-  path.resolve(__dirname, '../server.js'),
-  "module.exports = require('./dist/server');"
-);
+makeSubpackage('server', { mainDir: '../dist' });
