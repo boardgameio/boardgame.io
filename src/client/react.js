f0034dd97c6f8cd479d581b759c1b2059fdd85e7
flamecoals <flamecoals@users.noreply.github.com>
2020-04-19 17:30:07 -0700

    Trying to understand how plugin works...

--- src/client/react.js
+++ src/client/react.js
@@ -129,6 +129,7 @@ export function Client(opts) {
           reset: this.client.reset,
           undo: this.client.undo,
           redo: this.client.redo,
+          client: this.client,
           gameMetadata: this.client.gameMetadata,
         });
       }
