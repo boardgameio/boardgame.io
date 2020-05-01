792036aca78ab5b6f80a4e0d331aef5dc5bedae7
Adrian Gaudebert <adrian@gaudebert.fr>
2020-05-01 17:34:38 +0200

    Move tests to the right file.

--- src/plugins/main.test.js
+++ src/plugins/main.test.js
@@ -178,3 +178,70 @@ describe('actions', () => {
     expect(client.getState().plugins.nosetup.data).toEqual({ action: true });
   });
 });
+
+describe('plugins are accessible in events triggered from moves', () => {
+  test('turn/onEnd', () => {
+    const game = {
+      moves: {
+        stop: (G, ctx) => ctx.events.endTurn(),
+      },
+      turn: {
+        onEnd: (G, ctx) => {
+          G.onEnd = ctx.random.Die(1);
+        },
+      },
+    };
+
+    const client = Client({ game });
+    client.moves.stop();
+    expect(client.getState().G).toEqual({
+      onEnd: 1,
+    });
+  });
+
+  test('phase/onEnd', () => {
+    const game = {
+      moves: {
+        stop: (G, ctx) => ctx.events.endPhase(),
+      },
+      phases: {
+        first: {
+          start: true,
+          onEnd: (G, ctx) => {
+            G.onEnd = ctx.random.Die(1);
+          },
+        },
+      },
+    };
+
+    const client = Client({ game });
+    client.moves.stop();
+    expect(client.getState().G).toEqual({
+      onEnd: 1,
+    });
+  });
+
+  test('phase/onBegin', () => {
+    const game = {
+      moves: {
+        stop: (G, ctx) => ctx.events.setPhase('second'),
+      },
+      phases: {
+        first: {
+          start: true,
+        },
+        second: {
+          onBegin: (G, ctx) => {
+            G.onEnd = ctx.random.Die(1);
+          },
+        },
+      },
+    };
+
+    const client = Client({ game });
+    client.moves.stop();
+    expect(client.getState().G).toEqual({
+      onEnd: 1,
+    });
+  });
+});
