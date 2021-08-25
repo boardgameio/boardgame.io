a199d4783ae18b70983c03f388078c6858d72dde
Jonathan Li <comm@renderjon.li>
2021-08-25 16:36:27 -0700

    feat: Move game server route creation to run
    Allow user defined middleware to execute on boardgame.io defined routes.
    Previously created routes were instantiated before user could define
    middleware and thus they would not execute.

--- src/server/api.test.ts
+++ src/server/api.test.ts
@@ -8,9 +8,10 @@
 
 import request from 'supertest';
 import Koa from 'koa';
+import Router from 'koa-router';
 import * as dateMock from 'jest-date-mock';
 
-import { createRouter, configureApp } from './api';
+import { configureRouter, configureApp } from './api';
 import { ProcessGameConfig } from '../core/game';
 import { Auth } from './auth';
 import * as StorageAPI from './db/base';
@@ -70,7 +71,7 @@ class AsyncStorage extends StorageAPI.Async {
   }
 }
 
-describe('.createRouter', () => {
+describe('.configureRouter', () => {
   function addApiToServer({
     app,
     origins,
@@ -78,8 +79,9 @@ describe('.createRouter', () => {
   }: {
     app: Server.App;
     origins?: Parameters<typeof configureApp>[2];
-  } & Parameters<typeof createRouter>[0]) {
-    const router = createRouter(args);
+  } & Omit<Parameters<typeof configureRouter>[0], 'router'>) {
+    const router = new Router<any, Server.AppCtx>();
+    configureRouter({ router, ...args });
     configureApp(app, router, origins);
   }
 
