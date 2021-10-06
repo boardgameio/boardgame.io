7b151798e73a810fa819bd55388d58901cc66a60
delucis <swithinbank@gmail.com>
2021-10-06 23:41:59 +0100

    feat: Expose `getFilterPlayerView` from `internal` package

--- packages/internal.ts
+++ packages/internal.ts
@@ -9,6 +9,7 @@
 import { InitializeGame } from '../src/core/initialize';
 import { ProcessGameConfig } from '../src/core/game';
 import { CreateGameReducer } from '../src/core/reducer';
+import { getFilterPlayerView } from '../src/master/filter-player-view';
 import { Async, Sync } from '../src/server/db/base';
 import { Transport } from '../src/client/transport/transport';
 import { createMatch } from '../src/server/util';
@@ -21,4 +22,5 @@ export {
   InitializeGame,
   CreateGameReducer,
   createMatch,
+  getFilterPlayerView,
 };
